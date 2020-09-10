4bb8f06b9e47164d360a2ccb81ccb731c4e9b093
Nicolas Mandel <nicolasjohann.mandel@hdr.qut.edu.au>
2020-09-10 14:38:27 +1000

    [Nico] - Sim-Trial v0.5 - Color Lookup turned into DF

--- src/basic-sim.py
+++ src/basic-sim.py
@@ -6,40 +6,34 @@
 
     TODO: Indexing into the right higher level 
     block will require indexing like CUDA! blockDim.x * blockidx.X + ThreadIdx.x
-    # TODO: resolution dependant on distance
+
+    TODO: Color Mapping as a pandas dataframe - use the transpose method after creation
 
     # TODO: Color mapping of imshow of matplotlib through [this](https://matplotlib.org/3.1.0/api/_as_gen/matplotlib.pyplot.imshow.html)
 """
 
 import numpy as np
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+import pandas as pd
 
-class Colordict():
+
+def colordf():
     """
-        Helper Class to color code the class names for visualisation
+        Helper Function to turn the colors into a DatFrame - for ease of lookup with np
+        Returns a df object
     """
+    df = pd.DataFrame()
+    df["house"] = [0.921, 0.117, 0.137, 1.0]
+    df["pavement"] = [0.662, 0.674, 0.647, 1.0]
+    df["grass"] = [0.384, 0.976, 0.411, 1.0]
+    df["tree"] = [0.164, 0.576, 0.219, 1.0]
+    df["vehicle"] = [0.172, 0.533, 0.866, 1.0]
+    df["urban"] = [0.713, 0.207, 0.305, 1.0]
+    df["forest"] = [0.149, 0.380, 0.141, 1.0]
+    df["road"] = [0.49, 0.49, 0.49, 1.0]
+    df["grasslands"] = [0.713, 0.89, 0.631, 1.0]
+    return df
 
-    def __init__(self):
-        cdict = {}
-        cdict["house"] = tuple([0.921, 0.117, 0.137])   # Red
-        cdict["pavement"] = tuple([0.662, 0.674, 0.647])   # Gray
-        cdict["grass"] = tuple([0.384, 0.976, 0.411])
-        cdict["tree"] = tuple([0.164, 0.576, 0.219])
-        cdict["vehicle"] = tuple([0.172, 0.533, 0.866])
-        cdict["urban"] = tuple([0.713, 0.207, 0.305])
-        cdict["forest"] = tuple([0.149, 0.380, 0.141])
-        cdict["road"] = tuple([0.49, 0.49, 0.49])
-        cdict["grasslands"] = tuple([0.713, 0.89, 0.631])
-        self.cdict = cdict
-
-    def lookupcolor(self, cname):
-        """
-            Function to return the RGB representation of a color, specified by the class name
-        """
-        try:
-            return self.cdict[cname]
-        except LookupError as e:
-            raise e
 
 def createMap(n, m, l):
     """
@@ -91,21 +85,23 @@ def fillmap(gt, classlist, scenario):
     return gt
     
 
-def visualize_map(gtmap, cd):
+def visualize_map(gtmap, cd, returnarray=False):
     """
         Function to visualize the map using matplotlib and the color codes defined in cd  
     """
-    vis_map = np.empty((gtmap.shape[0],gtmap.shape[1], 3))
+    vis_map = np.empty((gtmap.shape[0],gtmap.shape[1], 4))
     # Fill the word with the color
     for j in range(gtmap.shape[0]):
         for k in range(gtmap.shape[1]):
             vis_map[j,k] = cd.lookupcolor(gtmap[j,k])
 
-    # visualize it with matplotlib imshow
-    fig, ax = plt.subplots()
-    ax.imshow(vis_map)
-    plt.show()
-
+    # visualize it with matplotlib imshow if returnarray is False, else return it
+    if not returnarray:
+        fig, ax = plt.subplots()
+        ax.imshow(vis_map)
+        plt.show()
+    else:
+        return vis_map
 
 def retrieveVisibleFields(wp, fov=1):
     """
@@ -121,7 +117,7 @@ def retrieveVisibleFields(wp, fov=1):
     # y_vals = np.arange(wp[1]-fov+1, wp[1]+fov+1)
     return x_min, x_max, y_min, y_max
 
-def obs_probab(classlist, maxim=0.8):
+def observation_probabilities(classlist, maxim=0.8):
     """
         Returns an array with the observation probabilities for each class.
         The observation probabilities are calculated using maxim as p(o|y) and a uniform distribution over all other values
@@ -133,7 +129,8 @@ def obs_probab(classlist, maxim=0.8):
     np.fill_diagonal(arr, maxim)
     off_diag = np.where(~np.eye(num_classes,dtype=bool))
     arr[off_diag] = conf_probab
-    return arr
+    df = pd.DataFrame(arr, columns=classlist, index=classlist)
+    return df
     
 def getpattern(x_max, y_max, fov=1):
     """
@@ -156,22 +153,37 @@ def updateprobab(obs, obs_probab, prior):
         obs_probab: probability of making the observations
         Returns: posterior over the map
     """
+    # Prior is a 3D array
+    post = np.empty_like(prior)
+    for i in obs.shape[0]:
+        for j in obs.shape[1]:
+            pr = prior[i,j]
+            vec = obs_probab.loc[obs[i,j],:]
+            po = vec*prior
+            po = po/po.sum()
+            post[i,j] = po
 
-    pass
-    # TODO: Continue here
-
+    return post
+            
+    
 def updateMap(x_min, x_max, y_min, y_max, posterior, lmap):
     """
         Function that takes the new probabilities with the observations and the original maps and update the function on the right indices
     """
     lmap[x_min:x_max, y_min:y_max] = posterior
 
-def makeObs(gt, obs_probab):
+def makeObs(gt, obs_probab, classlist):
     """
         Returns an observation based on the Ground truth and the Observation probability
     """
-    pass
-    # TODO:  
+
+    obs = np.empty((obs_probab.shape[0], obs_probab.shape[1]))
+    for i in range(gt.shape[0]):
+        for j in range(gt.shape[1]):
+            cl_name = gt[i,j]
+            prob = obs_probab.loc[cl_name,:]
+            obs[i,j] = np.random.choice(classlist, p=prob)
+    return obs
 
 def entropy(vec):
     """
@@ -181,6 +193,7 @@ def entropy(vec):
     return np.sum(np.dot(vec, lnvec)) * -1.0
 
 if __name__=="__main__":
+    cdf = colordf()
 
     max_map_size = 64
     blockDim = 2            # TODO: Indexing in the inverse hierarchical structure, similar to CUDA
@@ -189,47 +202,57 @@ if __name__=="__main__":
     fov = 2
 
     # n3 = m3 = max_map_size/(2)            Keep to 2 levels for now
-    l1_classlist = ["house", "pavement", "grass", "tree", "vehicle"]
-    l2_classlist = ["urban", "forest", "road", "grasslands"]
+    l1_classlist = np.asarray(["house", "pavement", "grass", "tree", "vehicle"])
+    # l2_classlist = ["urban", "forest", "road", "grasslands"]
     gtmap = createGTMap(n1, m1)            # Ground Truth Map
-    l1map = createMap(n1, m1, len(l1_classlist))            # belief map
+    l1map = createMap(n1, m1, l1_classlist.size)            # belief map
     
     # Making a Map
     cd = Colordict()
     gtmap = fillmap(gtmap, l1_classlist, 1)
-    visualize_map(gtmap, cd)
+    gt_vismap = visualize_map(gtmap, cd, True)
     
     # Observation probabilites and waypoints
-    obs = obs_probab(l1_classlist)
-    wps = getpattern(n1,m1, fov)      # Flight pattern
+    obs_prob = observation_probabilities(l1_classlist)
+    wps = getpattern(n1, m1, fov)      # Flight pattern
 
     # iterative procedure from here on out
-    last_wp = wps[-1]
-    x_min, x_max, y_min, y_max = retrieveVisibleFields(last_wp, fov=fov)
-    vis_fields = l1map[x_min:x_max, y_min:y_max]
-    
-    # TODO: Assign a prior distribution to the probability of observing the classes
+    # last_wp = wps[-1]
+    # x_min, x_max, y_min, y_max = retrieveVisibleFields(last_wp, fov=fov)
+    # vis_fields = l1map[x_min:x_max, y_min:y_max]
 
+    # Filling the PRIOR with values from the FUCKING SHIT FUCK FUCK FUCK
+    l1map.fill(1.0/l1_classlist.size)
+    l1map_vis = np.empty((l1map.shape[0], l1map.shape[1]), dtype="object")
+
+    # TODO: Visualisation, updating incrementally:
+        # Waypoints
+        # FoV
+        # Belief Map
+    fig, axes = plt.subplots(2, 2)
     for wp in wps:
         """
             This is where the iterative updates take place. Using the Ground Truth data (gt), the observation likelihood and the prior over the previous map
         """
+        # indices that are currently visible
         x_min, x_max, y_min, y_max = retrieveVisibleFields(wp, fov=fov)
-        
         gt = gtmap[x_min:x_max, y_min:y_max]    #  Ground Truth
         prior = l1map[x_min:x_max, y_min,y_max] # Prior
 
         # Make observation
-        obs = makeObs(gt, obs_probab)
+        obs = makeObs(gt, obs_prob, l1_classlist)
 
-        # TODO: how does the observed thing get incorporated into it?
-        posterior = updateprobab(obs, obs_probab, prior)
-
+        # how does the observed thing get incorporated into it?
+        posterior = updateprobab(obs, obs_prob, prior)
         # Re-incorporate the information into the map
+        l1map[x_min:x_max, y_min:y_max] = posterior
 
 
+        # Plotting section
+        axes[0].imshow(gt_vismap)
+        axes[0].set(title="Ground Truth Map")
+
+        # clear the plots
+         
         
-
-
-
     print("Test Done")
